26801ae3e1b43509f76a46227daab0b1cae15cd5
Rajkumar1273 <110664973+Rajkumar1273@users.noreply.github.com>
2022-09-13 13:36:19 +0530

    first commit to move the code

--- cicd-server.tf
+++ cicd-server.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "cicd12" {
+resource "aws_instance" "cicd-server1" {
   ami                         = "ami-0b89f7b3f054b957e"
   instance_type               = "t2.micro"
   vpc_security_group_ids      = [aws_security_group.apache.id]
@@ -6,25 +6,13 @@ resource "aws_instance" "cicd12" {
   associate_public_ip_address = true
 #   subnet_id                   = aws_subnet.vpc.id
   key_name                    = aws_key_pair.deployer.id
-  tags = {
-    name      = "stage-cicd12"
-    terraform = "true"
-  }
-}
-resource "aws_instance" "apache-server" {
-  ami                         = "ami-0b89f7b3f054b957e"
-  instance_type               = "t2.micro"
-  vpc_security_group_ids      = [aws_security_group.apache.id]
-#   vpc_id                      = "vpc-03ed34629d50653ca"
-  associate_public_ip_address = true
-#   subnet_id                   = aws_subnet.vpc.id
-  key_name                    = aws_key_pair.deployer.id
-user_data =<<-EOF
+  user_data =<<-EOF
+#! /bin/bash
 yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
 yum install git -y
-git clone  https://github.com/rajkumargit12/marvell.git
+git clone git@github.com:rajkumargit12/mahammad.git
 
 wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
@@ -36,8 +24,32 @@ systemctl start jenkins
 systemctl enable jenkins
 systemctl status jenkins
   EOF
+
   tags = {
-    name      = "stage-cicd12"
+    name      = "cicd-server1"
+    terraform = "true"
+  }
+}
+resource "aws_instance" "apache-server1" {
+  ami                         = "ami-0b89f7b3f054b957e"
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.apache.id]
+#   vpc_id                      = "vpc-03ed34629d50653ca"
+  associate_public_ip_address = true
+#   subnet_id                   = aws_subnet.vpc.id
+  key_name                    = aws_key_pair.deployer.id
+user_data = <<-EOF
+  #! /bin/bash
+  sudo su -
+   yum install httpd -y
+   yum update httpd -y
+   systemctl start httpd
+   sytemctl enable httpd
+   systemctl status httpd
+    EOF
+
+  tags = {
+    name      = "apache-server1"
     terraform = "true"
   }
 }
